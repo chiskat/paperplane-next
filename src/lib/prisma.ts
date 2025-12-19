2f31143cccbcb47cabad9f4f58fa2a2b038b2eae
jia-niang <1@paperplane.cc>
2025-12-20 05:33:20 +0800

    chore(build): 避免构建期间出现 prisma 和 better-auth 的报错信息

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,7 +2,9 @@ import 'server-only'
 
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = (globalThis.prisma as PrismaClient) || new PrismaClient()
+export const prisma =
+  (globalThis.prisma as PrismaClient) ||
+  (process.env.CI ? (null as unknown as PrismaClient) : new PrismaClient())
 
 if (process.env.NODE_ENV !== 'production') {
   globalThis.prisma = prisma
