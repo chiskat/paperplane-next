90ba30a877d0b34e118fdee691f19c6bec2b3fc8
jia-niang <1@paperplane.cc>
2025-12-27 03:48:14 +0800

    feat(registry): 现在制品库页面有默认的 loading 样式

--- src/app/registry/page.tsx
+++ src/app/registry/page.tsx
@@ -1,5 +1,12 @@
+'use client'
+
 import { redirect } from 'next/navigation'
+import { useEffect } from 'react'
+
+import Loading from '@/components/layouts/Loading'
 
 export default function RegistryPage() {
-  redirect('/registry/docker-mirror')
+  useEffect(() => void redirect('/registry/docker-mirror'), [])
+
+  return <Loading className="w-full" />
 }
